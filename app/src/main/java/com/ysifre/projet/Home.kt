21b3b657ed70b6be4ee6b2ef92597d37ff6a8690
debrasnicolas <nicodebras17@gmail.com>
2023-03-02 09:28:18 +0100

    update config to run

--- app/src/main/java/com/ysifre/projet/Home.kt
+++ app/src/main/java/com/ysifre/projet/Home.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import fake.`package`.name.`for`.sync.R
 
 class Home: Fragment() {
     private lateinit var validateButton: Button

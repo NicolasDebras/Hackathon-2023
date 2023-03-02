7251db3597cbaea7bae8c89365c9d12ae86d6d01
YvanSifre <79469794+YvanSifre@users.noreply.github.com>
2023-03-02 09:15:11 +0100

    first commit

---
+++ app/src/main/java/com/ysifre/projet/Home.kt
@@ -0,0 +1,29 @@
+package com.ysifre.projet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import fake.`package`.name.`for`.sync.R
+
+class Home: Fragment() {
+    private lateinit var validateButton: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.home, container, false).apply {
+            validateButton = findViewById<Button>(R.id.validateButton) as Button
+            validateButton.setOnClickListener {
+                findNavController().navigate(
+                    HomeDirections.actionHomeToListTrajets()
+                )
+            }
+        }
+    }
+}
